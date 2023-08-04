69f6a88f2475850d282f8688b183cf33bef37571
Gabriel Ferraz <gabrielferraz97@outlook.com>
2023-08-04 18:04:44 -0300

    Fixing

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,14 +3,13 @@ import styles from "./header.module.css"
 //O nome que virá logo após "styles" será o nome da função a ser importada
 import IgniteLogo from '../assets/ignite-logo.svg';
 
-
 export function Header() {
     return (
         <header className={styles.header} >
         <img src={IgniteLogo} alt="Logotipo do Ignite" />
     
         </header>
-        
+       
     )
 }
 //Acima, quando importamos Ignite logo, note que ainda que a importação seja de um arquivo css, tratamos como um elemento javascript, desta forma, ao utilizarmos o recurso no código, devemos colocar entre curly brackets, assim como qualquer outro elemento javacript dentro do XML
